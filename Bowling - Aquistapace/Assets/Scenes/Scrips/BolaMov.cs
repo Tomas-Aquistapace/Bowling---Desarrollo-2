22062dbd9da4afdc0873e7016d43eb660397badc
Tomas <Aquista35@gmail.com>
2021-04-07 23:54:30 -0300

    Movimiento agregado a la bola

---
+++ Bowling - Aquistapace/Assets/Scenes/Scrips/BolaMov.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaMov : MonoBehaviour
+{
+    [Header("Bola Data")]
+    public float speed = 1;
+    public float force = 1;
+    //private float FORCE_INCREMENT = 100;
+
+    [Header("Limits in X")]
+    public float minX;
+    public float maxX;
+
+    [HideInInspector]
+    public Rigidbody rig;
+    private float horizontal;
+
+    public bool launch;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        rig.useGravity = false;
+        launch = false;
+    }
+    
+    void Update()
+    {
+        if (launch == false)
+            horizontal = Input.GetAxis("Horizontal");
+
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementHor = transform.right * horizontal * speed * Time.deltaTime;
+        
+        rig.MovePosition(rig.position + movementHor);
+        rig.position = new Vector3(Mathf.Clamp(rig.position.x, minX, maxX), rig.position.y, rig.position.z);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown("space") && launch == false)
+        {
+            rig.AddForce(transform.forward * force, ForceMode.VelocityChange);
+            rig.useGravity = true;
+
+            launch = true;
+        }
+    }
+}
